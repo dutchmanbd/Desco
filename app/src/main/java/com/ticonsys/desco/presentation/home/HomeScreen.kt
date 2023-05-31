78eabc7d4227a2d9271e198da2ff90c6d6afe9b2
jewelrana <jewel@ticonsys.com>
2023-05-31 13:07:25 +0600

    feat: Design updated

--- app/src/main/java/com/ticonsys/desco/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/ticonsys/desco/presentation/home/HomeScreen.kt
@@ -5,10 +5,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -16,8 +25,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.ticonsys.desco.R
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
@@ -49,12 +63,12 @@ fun HomeScreen() {
             webViewClient = object : WebViewClient() {
                 override fun onPageFinished(view: WebView?, url: String?) {
                     super.onPageFinished(view, url)
-//                    view?.evaluateJavascript(
-//                        "document.querySelector('form button').style.display = 'none'"
-//                    ) {}
                     view?.evaluateJavascript(
-                        "document.querySelector('.card-group').style.display = 'none'"
+                        "document.querySelector('form button').style.display = 'none'"
                     ) {}
+//                    view?.evaluateJavascript(
+//                        "document.querySelector('.card-group').style.display = 'none'"
+//                    ) {}
                 }
             }
             visibility = View.GONE
@@ -63,25 +77,42 @@ fun HomeScreen() {
         it.loadUrl(url)
     })
 
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
+    if (!hasSubmitted) {
 
-        if (!hasSubmitted) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                TextField(
-                    value = accountNumber,
-                    onValueChange = viewModel::updateAccountNumber
-                )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_desco),
+                contentDescription = "Desco Logo"
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "You can check your balance",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.W700
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            OutlinedTextField(
+                value = accountNumber,
+                onValueChange = viewModel::updateAccountNumber,
+                label = {
+                    Text(text = "Account/Meter No")
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Person, contentDescription = "Person")
+                },
+                singleLine = true,
+            )
 
-                Button(
-                    onClick = {
-                        webView?.evaluateJavascript(
-                            """
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {
+                    webView?.evaluateJavascript(
+                        """
                                    el= document.querySelector('[placeholder="Account/Meter No"]')
                                    el.value = '$accountNumber'
 
@@ -91,21 +122,28 @@ fun HomeScreen() {
 
                                    document.querySelector('form button').click()
                                 """.trimIndent()
-                        ) {
+                    ) {
 
-                        }
+                    }
 
-                        viewModel.submit(true)
-                        webView?.visibility = View.VISIBLE
-                    },
-                    enabled = accountNumber.isNotEmpty()
-                ) {
-                    Text(text = "Submit")
-                }
+                    viewModel.submit(true)
+                    webView?.visibility = View.VISIBLE
+                },
+                enabled = accountNumber.isNotEmpty()
+            ) {
+                Text(text = "Submit")
             }
+
+
         }
-
-
+//        Text(
+//            modifier = Modifier
+//                .align(alignment = Alignment.BottomEnd)
+//                .padding(16.dp),
+//            text = "* Unofficial app",
+//            fontSize = 12.sp,
+//            fontWeight = FontWeight.W300
+//        )
     }
 
 }
